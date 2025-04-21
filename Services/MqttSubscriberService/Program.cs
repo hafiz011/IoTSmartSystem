067f7ef5172b60d3198804d37fbbe5bfdd93c277
hafiz011 <hrhafij8@gmail.com>
2025-04-21 16:50:56 +0600

    Update MQTT Cliend

--- Services/MqttSubscriberService/Program.cs
+++
@@ -1,9 +0,0 @@
-using MqttSubscriberService;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<MqttSubscriberService.Worker>();
-
-builder.Services.AddHostedService<MqttSubscriberService>();
-
-var host = builder.Build();
-host.Run();
