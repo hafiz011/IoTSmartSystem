7b4a52c37a0fc71454806220139d876fa195a653
hafiz011 <hrhafij8@gmail.com>
2025-04-28 17:01:34 +0600

    update gRPC update

--- Services/MqttSubscriberService/LogSensorData.cs
+++ Services/MqttSubscriberService/LogSensorData.cs
@@ -1,28 +1,26 @@
 ﻿using Grpc.Net.Client;
+using SensorDataLoggingService.Grpc; // This is your generated proto classes
 using MqttSubscriberService.Models;
-using SensorDataLoggingService.Grpc;
+
 namespace MqttSubscriberService
 {
     public class LogSensorData
     {
         public async Task<LogSensorDataResponse> SensorData(SensorDataDto data)
         {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5004");
+            var client = new SensorDataLogger.SensorDataLoggerClient(channel);
 
-                using var channel = GrpcChannel.ForAddress("https://localhost:5007");
-                var client = new SensorDataLoggingService.SensorDataLoggerService.LogSensorDataC(channel);
+            var request = new SensorDataRequest
+            {
+                DeviceId = data.DeviceId,
+                ReceivedAt = data.ReceivedAt.ToString("o"),
+                Value = data.Value,
+                Type = data.Type
+            };
 
-                var forwardRequest = new SensorDataDto
-                {
-                    DeviceId = data.DeviceId,
-                    Value = data.Value,
-                    Type= data.Type,
-                    ReceivedAt = data.ReceivedAt
-                };
-
-                var response = await client.LogSensorData(forwardRequest);
-
-                return response.Success;
-            
+            var response = await client.LogSensorDataAsync(request);
+            return response;
         }
     }
 }
