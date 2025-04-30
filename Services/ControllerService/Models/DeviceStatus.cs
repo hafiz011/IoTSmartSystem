3afd843132196af24848f50748220093ccdc3baf
hafiz011 <hrhafij8@gmail.com>
2025-04-30 18:06:26 +0600

    update model

--- Services/ControllerService/Models/DeviceStatus.cs
+++ Services/ControllerService/Models/DeviceStatus.cs
@@ -5,23 +5,25 @@ namespace ControllerService.Models
 {
     public class DeviceStatus
     {
+
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
         public string DeviceId { get; set; }
         public string DeviceName { get; set; }
-        public string Location { get; set; }
 
-        public List<DeviceOutput> Outputs { get; set; }
+        [BsonRepresentation(BsonType.Document)]
+        public Dictionary<string, DeviceOutput> Outputs { get; set; }
+
+        public bool IsManualOverride { get; set; } // Is the device in manual mode?
 
         public DateTime LastUpdated { get; set; }
     }
 
     public class DeviceOutput
     {
-        public string Type { get; set; } // AC, Light, Fan
-        public string Status { get; set; } // On / Off
-        public int Value { get; set; } // Temp for AC, Level 1–10 for Light/Fan
+        public string Status { get; set; }  // "On", "Off"
+        public int Value { get; set; }      // For example: brightness level
     }
 }
